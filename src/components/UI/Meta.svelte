be8a774c4ad595ff08f9a716ef7a9a05dac3feee
SamTF <elseban88@gmail.com>
2023-10-06 17:26:45 +0200

    Initial commit

---
+++ src/components/UI/Meta.svelte
@@ -0,0 +1,30 @@
+<!-- Metadata Component -->
+
+<!-- JS -->
+<script>
+    export let title = ''
+    export let description = ''
+    export let thumbnail = '/images/logo.webp'
+    export let url = 'https://sam.freelancepolice.org/'
+    export let colour = '#FF0062'
+    export let bigMode = false
+</script>
+
+<!-- META -->
+<svelte:head>
+    <!-- Page Title -->
+    <title>{title == '' ? 'Web Games by Sam Casaleiro' : `${title} // Sam Casaleiro`}</title>
+
+    <!-- Link Embed Stuff -->
+    <meta property="og:site_name"   content="grotto.builders">      <!-- Top of the embed -->
+    <meta property="og:title"       content={title}>                <!-- Big Bold Letters -->
+    <meta property="og:description" content={description}>          <!-- Subtitle, smaller, under title -->
+    <meta property="og:image"       content={thumbnail}>            <!-- Cool lil thumbnail to the side! :) -->
+    <meta property="og:url"         content={url}>                  <!-- Link when you click on the title -->
+    <meta name="theme-color"        content={colour}>               <!-- Embed sidebar colour! -->
+
+    {#if bigMode}
+        <meta name="twitter:card"   content="summary_large_image">
+    {/if}
+    
+</svelte:head>
---
+++ src/routes/hatboy/+page.svelte
@@ -0,0 +1,44 @@
+<!-- Page for the HAT BOY game -->
+
+<!-- JS -->
+<script>
+    import Meta from "../../components/UI/Meta.svelte";
+</script>
+
+<!-- META -->
+<Meta
+    title={"Hat Boy"}
+    description={"Strange surroundings. Sinister creatures. Suspicious skulls. And lots o' hats!"}
+    thumbnail={""}
+/>
+
+<!-- HTML -->
+<main>
+    <h1>Hat Boy: The Beginning</h1>
+
+    <iframe
+        src="/games/hat-boy/hat_boy__the_beginning.html"
+        class="game-frame"
+        title="GAME"
+        id="game_drop"
+        frameborder="0"
+        scrolling="no"
+        allowfullscreen="true"
+        allowtransparency="true"
+        webkitallowfullscreen="true"
+        msallowfullscreen="true"
+        allow="autoplay; fullscreen *; geolocation; microphone; camera; midi; monetization; xr-spatial-tracking; gamepad; gyroscope; accelerometer; xr; cross-origin-isolated"
+    />
+</main>
+
+<!-- CSS -->
+<style>
+    main {
+        display: grid;
+        place-items: center;
+    }
+    iframe {
+        width: 40vw;
+        height: 40vw;
+    }
+</style>
