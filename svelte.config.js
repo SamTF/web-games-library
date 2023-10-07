5387012e993110cca27bc93ab0d89ef43d2fb863
SamTF <elseban88@gmail.com>
2023-10-07 18:00:19 +0200

    Fixed adapter options

--- svelte.config.js
+++ svelte.config.js
@@ -7,17 +7,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			// static adapter options
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
 		alias: {
 			'$components': 'src/components',
 			'$components/*': 'src/components/*',
-		},
-		// static adapter options
-		pages: 'build',
-		assets: 'build',
-		fallback: undefined,
-		precompress: false,
-		strict: true
+		}
 	}
 };
 
