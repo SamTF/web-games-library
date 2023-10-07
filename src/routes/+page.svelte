2ae60bc79e1d3a976a33bb983ddfaaba3a040ffa
SamTF <elseban88@gmail.com>
2023-10-07 17:03:04 +0200

    Placeholder landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- JS -->
+<script>
+    import Meta from "$components/UI/Meta.svelte"
+</script>
+
+<!-- META -->
+<Meta
+    title={"Hat Boy"}
+    description={"Strange surroundings. Sinister creatures. Suspicious skulls. And lots o' hats!"}
+    thumbnail={"/games/hat-boy/icon.gif"}
+/>
+
+<!-- HTML -->
+<main class="center">
+    <div>
+        <h1>Sam's Web Games</h1>
+        <br><br>
+        <p>Right now this website only has <a href="/hat-boy" class="accent"><i><b>Hat Boy</b></i></a>, but in the future it will house many more games! (hopefully!!)</p>
+    </div>
+</main>
+
+<!-- CSS -->
+<style>
+    main {
+        height: 100%;
+        width: 100%;
+    }
+</style>
