b45b6054f6a74e2147f71cbd907274e5b6c4d0cd
SamTF <elseban88@gmail.com>
2023-10-07 19:58:30 +0200

    Fixed meta subtitle

--- src/components/UI/Meta.svelte
+++ src/components/UI/Meta.svelte
@@ -16,7 +16,7 @@
     <title>{title == '' ? 'Web Games by Sam Casaleiro' : `${title} // Sam Casaleiro`}</title>
 
     <!-- Link Embed Stuff -->
-    <meta property="og:site_name"   content="grotto.builders">      <!-- Top of the embed -->
+    <meta property="og:site_name"   content="Sam's Web Games">      <!-- Top of the embed -->
     <meta property="og:title"       content={title}>                <!-- Big Bold Letters -->
     <meta property="og:description" content={description}>          <!-- Subtitle, smaller, under title -->
     <meta property="og:image"       content={thumbnail}>            <!-- Cool lil thumbnail to the side! :) -->
