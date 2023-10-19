292073543254ddcbb95a9bf8dfac6e831215e10e
SamTF <elseban88@gmail.com>
2023-10-19 13:51:06 +0200

    Moved access codes to ENV file!

--- src/routes/api/auth/[code]/+server.js
+++ src/routes/api/auth/[code]/+server.js
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit'
 
 export async function GET({ url, params }) {
-    const validCodes = ['SAM-666-WTF', 'HAT-420-NET', 'HAT-007-TEO', 'HAT-111-MAX', 'HAT-197-JOE']
+    const validCodes = import.meta.env.VITE_ACCESS_CODES
     
     const userCode = params.code
 
